ed371bf1971a281d3945657f7213e9cfac0db196
peipei2 <122847549+peipei2@users.noreply.github.com>
2023-08-21 22:38:19 +0800

    Add files via upload

---
+++ js/contact_me.js
@@ -0,0 +1,66 @@
+$(function() {
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var tel = $("input#tel").val();
+            var message = $("textarea#message").val();
+            if (tel == "" || !(/^1(3|4|5|6|7|8|9)\d{9}$/.test(tel))) {
+                $('#success').html("<div class='alert alert-success'>tel error!!!</div>");
+                $("input#tel").focus();
+                return;
+            }
+            if (message == "" || message == null) {
+                $('#success').html("<div class='alert alert-success'>content is null!!!</div>");
+                $("input#tel").focus();
+                return false;
+            }
+            $.ajax({
+                url: "url",
+                type: "post",
+                data: { tel: tel, content: message },
+                success: function (data) {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
